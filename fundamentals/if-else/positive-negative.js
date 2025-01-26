d404547bacc3dc41f701c78f841bc0b1756f2260
marcodialvva <marcodialvva@gmail.com>
2025-01-25 23:41:58 -0600

    feat(if-else): add positve-negavite exercise

---
+++ fundamentals/if-else/positive-negative.js
@@ -0,0 +1,24 @@
+/*
+Escribe un programa simple en JavaScript que evalúe el valor de una variable llamada 'n'.
+Verifica si el número es positivo, negativo o cero, y muestra un mensaje correspondiente
+basado en la siguiente evaluación:
+Si el número es mayor que 0, imprime 'Positivo'.
+Si el número es menor que 0, imprime 'Negativo'.
+Si el número es exactamente 0, imprime 'Cero'•
+Si la variable no es un número, imprime un mensaje de error: 'Por favor, introduce un número válido'
+
+*/
+
+const prompt = require (`prompt-sync`)()
+const n = parseFloat(prompt (`Enter any numer:`))
+
+
+if (n > 0){
+  console.log(`Positivo`)
+} else if ( n === 0) {
+   console.log(`cero`)
+} else if (n < 0){
+   console.log(`Negativo`) 
+} else {
+   console.log(`Por favor, introduce un número válido`)
+}
