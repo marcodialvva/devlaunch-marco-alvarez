b989053cc02e500e82ce02cd1416512ee6ebf15c
marcodialvva <marcodialvva@gmail.com>
2025-01-31 17:02:19 -0600

    chore(job-payment) add job-payment exercise

--- fundamentals/functions/test.js
+++ fundamentals/functions/test.js
@@ -1,51 +1,59 @@
-const prompt = require(`prompt-sync`)()
+/*
 
-const minRecharge = 20
-const maxRecharge = 80
-const dupRecharge = 38
-const minYears = 1
-const yearsMegaPromo = 3
+En Respuestal S.A.  hay proceso donde cada trabajador debe registrar las horas de entrada y salida diariamente. De esta forma, al final de cada dia, 
+su empleador sabrá cuánto tiene que pagarles por sus horas trabajadas.
+
+Pero, si una persona trabajó más de ocho horas, se le debe pagar un costo extra por las horas adicionales, que es igual a 1.5 veces el monto de su tarifa por hora.
+
+
+Haz un programa que, recibiendo la tarifa por hora, la hora de entrada y la hora de salida, imprima el monto total de dinero que ese empleado recibió ese día.
+
+Tarifa por hora: $10
+Hora de entrada: 9:00
+Hora de salida: 18:00
+ Respuesta  $95
+
+
+*/
+
+const prompt = require(`prompt-sync`)()
+const WORKED_DAY = 8
+const EXTRA_RATE = 1.5
+
 
 
 function main() {
-    const clientName = prompt(`Add the client name: `)
-    const sDate = prompt(`What is your subscription date (use this format YYYY/MM/DD): `)
-    const rechargeAmount = parseFloat(prompt(`Ingrese el monto que desea recargar: $ `))
-    const recharge = calculateRecharge (sDate, rechargeAmount, clientName)
+  const hourlyRate = parseFloat (prompt (`HOURLY RATE: $ `))
+  const starTime = prompt(`START AT: `)
+  const finishTime = prompt(`END AT: `)
+  const payment = paycheck(starTime, finishTime, hourlyRate)
 
- return recharge
+  return console.log(`Tarifa diaria $10
+Hora de entrada:${starTime}  
+Hora de salida: ${finishTime} 
+Payment :$${payment} `)
+  
 }
+
 main()
 
-function calculateRecharge (sDate, rechargeAmount, clientName) {
+function paycheck(starTime, finishTime, hourlyRate) {
+  const [startHour] = starTime.split (':')
+  const [finishHour] = finishTime.split (':') 
+  
+  const workedHours = finishHour - startHour
+  const extraTime = workedHours - WORKED_DAY
+  const regularPay = (workedHours-extraTime) * hourlyRate
+  const extraRate = hourlyRate * EXTRA_RATE
+  const extrapayment = extraTime * extraRate
 
-    const miliSec = 1000
-    const sec = 60
-    const minutes = 60
-    const hour = 24
-    const subscriptionDate = new Date(sDate);
-    const currentDate = new Date();
-    const diffMs = currentDate - subscriptionDate;
-    const totalDays = (diffMs / (miliSec * sec * minutes * hour));
-    const yearsSubscribed = (totalDays / 365);
-
-    const sSRecharge = rechargeAmount + rechargeAmount
-    const mRRecharge = rechargeAmount + rechargeAmount + rechargeAmount
-
-
-    if (yearsSubscribed < minYears || rechargeAmount < minRecharge || rechargeAmount > maxRecharge) {
-        return console.log(`SORRY ${clientName} THIS PROMOTION IS NOT VALID FOR YOU !!!`)
-
-    } else if (yearsSubscribed < yearsMegaPromo && rechargeAmount >= dupRecharge) {
-        return console.log(`SORRY ${clientName}  THIS PROMOTION IS NOT VALID FOR YOU !!!`)
-
-    } else if (yearsSubscribed < yearsMegaPromo && rechargeAmount <= dupRecharge) {
-        return console.log(`CONGRATS ${clientName}, YOU GET THE SUPER SAVER PROMOTION. 
-        Your recharge will be:$ ${sSRecharge}`)
-
-    } else if (yearsSubscribed >= yearsMegaPromo) {
-        return console.log(`CONGRATS ${clientName}, YOU GET THE MEGARECHARGE PROMOTION. 
-        Your recharge will be:$ ${mRRecharge}`)
-    }
+  if (workedHours === 0) {
+    return 0
+  }
+  else if (workedHours <= WORKED_DAY) {
+    return regularPay
+  } else if (workedHours > WORKED_DAY ) {
+return regularPay+extrapayment
+  }
 
 }
