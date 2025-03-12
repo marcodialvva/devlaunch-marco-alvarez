f48583bc57553d1497a982353b85d393583451bf
marcodialvva <marcodialvva@gmail.com>
2025-03-12 12:16:32 -0600

    chore(arrays): add dna1 exercise

--- data-structures/arrays/dna1.ts
+++ data-structures/arrays/dna1.ts
@@ -7,7 +7,6 @@ function isADnaMatch (dnaString2: string, sequence: string ): void {
 
     const isAMatch1 = dnaString2.includes(sequences)
 
-
     if (isAMatch1 === true) {
         console.log (`POSITIVE: ${sequences} is present in the DNA STRING ${dnaString2}`)
 
